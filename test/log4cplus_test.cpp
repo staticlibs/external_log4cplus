50173dab1c6c3be80f6f0ea2e1cebf0f80970cc5
alex <alex@staticlibs.net>
2015-02-20 12:30:15 +0000

    cmake script rewritten

--- test/log4cplus_test.cpp
+++ test/log4cplus_test.cpp
@@ -5,10 +5,31 @@
  * Created on February 19, 2015, 11:05 PM
  */
 
+#include <log4cplus/logger.h>
+#include <log4cplus/fileappender.h>
+#include <log4cplus/layout.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/loggingmacros.h>
 
-// todo
+namespace lg = log4cplus;
+
+const int LOOP_COUNT = 20000;
+
 int main() {
+    lg::initialize();
+    lg::SharedAppenderPtr append_1(new lg::DailyRollingFileAppender("Test.log"));
+    auto pattern = "%d{%Y-%m-%d %H:%M:%S,%q} [%-5p %-10.10T %-30.30c] %m%n";
+    append_1->setLayout(std::auto_ptr<lg::Layout>(new lg::PatternLayout(pattern)));
+    lg::Logger::getRoot().addAppender(append_1);
+    lg::Logger::getRoot().setLogLevel(lg::ALL_LOG_LEVEL);
+    lg::Logger subTest = lg::Logger::getInstance("test.subtest");
 
+    for (int i = 0; i < LOOP_COUNT; ++i) {
+        LOG4CPLUS_TRACE(subTest, "Entering loop #" << i);
+        LOG4CPLUS_DEBUG(subTest, "Entering loop #" << i);
+        LOG4CPLUS_INFO(subTest, "Entering loop #" << i);
+    }
+    
     return 0;
 }
 
